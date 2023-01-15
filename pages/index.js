e507c6b3676b49477a4ef86a81880196c94e6670
Robert Jaramillo <robert.jaramillo0484@gmail.com>
2023-01-14 22:13:19 -0500

    Base lay out and general functionality.  CHAT-GPT not yet integrated.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,61 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+  
   return (
     <div className="root">
       <Head>
         <title>GPT-3 Writer | buildspace</title>
       </Head>
       <div className="container">
+        
         <div className="header">
+        
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Prompt Maker</h1>
+            <r>The AI art prompt generator</r>
+            
           </div>
+        
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Enter the subject to create an AI prompt about</h2>
           </div>
+        
         </div>
+        
+        <div className="prompt-container">
+        <textarea
+          className="prompt-box"
+          placeholder="start typing here"
+          value={userInput}
+          onChange={onUserChangedText}
+        />;
+  
+        {/* Button  */}
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={null}>
+            <div className="generate">
+              <p>Generate</p>
+           </div>
+          </a>
+        </div>
+</div>
+
+
       </div>
+      
+
+      
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
